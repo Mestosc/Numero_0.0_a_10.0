d6c14f2c483c37aef2a26ed783c732a77e369ace
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-19 16:42:36 +0100

    Hacer que no se pueda introducir numeros fuera del intervalo

--- src/AdivinaNumero.py
+++ src/AdivinaNumero.py
@@ -19,13 +19,17 @@ def mayor_menor(num_aleatorio, numero_input):
         return "menor"
 intentos = 3
 hechos = 0
-numero_aleatorio = round(random.uniform(0,10),1)
+segundo_numero_inter = 10
+numero_aleatorio = round(random.uniform(0,segundo_numero_inter),1)
 print("Este es un juego de adivinar numeros del 0 al 10, tiene un total de " + str(intentos) + " intentos")
 while hechos<intentos:
     numero = float(input("Introduzca un numero:"))
     if comparar_numeros(numero_aleatorio, numero):
         print("Has ganado en un total de " + str(hechos+1) + " intentos")
         break
+    elif numero>segundo_numero_inter:
+        print("Has introducido un numero no valido por favor vuelve a introducir")
+        continue
     else:
         hechos += 1
         print("Intentalo otra vez el numero que has introducido es " + mayor_menor(numero_aleatorio, numero) + "\nTe quedan " + str(intentos-hechos) + " intentos")
